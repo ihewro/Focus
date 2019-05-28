e12bfa6f206b226dcca58ee294011dcdd6e47848
hewro <ihewro@163.com>
2019-05-28 16:14:44 +0800

    增加页面滚动条

--- app/src/main/java/com/ihewro/focus/fragemnt/setting/GestureFragment.java
+++ app/src/main/java/com/ihewro/focus/fragemnt/setting/GestureFragment.java
@@ -4,6 +4,7 @@ package com.ihewro.focus.fragemnt.setting;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.SwitchPreferenceCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,9 @@ import com.ihewro.focus.R;
  */
 public class GestureFragment extends SettingFragment {
 
+    private SwitchPreferenceCompat notOpenClick;
+    private SwitchPreferenceCompat notStar;
+    private SwitchPreferenceCompat notToTop;
 
 
     @Override
@@ -26,16 +30,21 @@ public class GestureFragment extends SettingFragment {
 
     @Override
     public void initPreferenceComponent() {
-
+        notOpenClick = (SwitchPreferenceCompat) findPreference(getString(R.string.pref_key_not_pull_to_open_link));
+        notStar = (SwitchPreferenceCompat) findPreference(getString(R.string.pref_key_not_double_click_to_star));
+        notToTop = (SwitchPreferenceCompat) findPreference(getString(R.string.pref_key_not_double_click_to_top));
     }
 
     @Override
     public void initPreferencesData() {
+        //恢复数据
+
 
     }
 
     @Override
     public void initListener() {
+        //保存数据
 
     }
 
